baf114edfb0c01fe9b2feb4befc38ad530ffb9f7
PaigeC163 <paige.clay163@gmail.com>
2025-06-10 19:11:21 +1200

    stuff3

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,8 @@
-import { HashRouter } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom'; 
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
